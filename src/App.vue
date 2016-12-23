7f98c10f2cdd108b611638f3485ea6334de0657c
infernalslam <infernal-slam@hotmail.com>
2016-12-23 22:07:53 +0700

    123

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <input type="text" v-model="name" placeholder="input name">
+    <button @click="add()">ADD User</button> <br>
+    <hr>
+    search: <input type="text" v-model="search">
+    <div v-for="(e, key) in listFilter">
+      {{ e.name }} <button @click="del(key)">x</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+  },
+  data () {
+    return {
+      list: [
+        {
+          name: 'mint'
+        },
+        {
+          name: 'tak'
+        }
+      ],
+      search: '',
+      name: ''
+    }
+  },
+  computed: {
+    listFilter () {
+      let text = this.search.trim()
+      return this.list.filter(item => {
+        return item.name.indexOf(text) > -1
+      })
+    }
+  },
+  methods: {
+    add () {
+      this.list.push({ name: this.name })
+    },
+    del (id) {
+      console.log(id)
+      this.list.splice(id, 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
